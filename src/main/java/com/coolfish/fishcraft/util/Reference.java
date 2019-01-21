69ce5fcb074f41b85b501f91ea946334c72af263
1coolfish <44551877+1coolfish@users.noreply.github.com>
2019-01-21 23:03:17 +0000

    Add files via upload

---
+++ src/main/java/com/coolfish/fishcraft/util/Reference.java
@@ -0,0 +1,11 @@
+package com.coolfish.fishcraft.util;
+
+public class Reference {
+
+	public static final String MOD_ID = "fishcraftmod";
+	public static final String NAME = "FishCrraft";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+	public static final String CLIENT_PROXY_CLASS = "com.coolfish.fishcraft.proxy.ClientProxy";
+	public static final String COMMON_PROXY_CLASS = "com.coolfish.fishcraft.proxy.CommonProxy";
+}
